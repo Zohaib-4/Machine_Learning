c5c35b48298a44c96a0c4955a175b2d6d9307e36
Zohaib <sameer.zohaib363@gmail.com>
2024-11-28 13:39:04 +0500

    /

--- Diabetes_Prediction/Diabetes_Django/diabetes_app/models.py
+++ Diabetes_Prediction/Diabetes_Django/diabetes_app/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class PatientData(models.Model):
-    pregnancies = models.PositiveIntegerField(help_text="Number of pregnancies")
-    glucose = models.PositiveIntegerField(help_text="Plasma glucose concentration (mg/dL)")
-    blood_pressure = models.PositiveIntegerField(help_text="Diastolic blood pressure (mm Hg)")
-    skin_thickness = models.PositiveIntegerField(help_text="Triceps skin fold thickness (mm)")
-    insulin = models.PositiveIntegerField(help_text="2-Hour serum insulin (mu U/ml)")
-    bmi = models.FloatField(help_text="Body Mass Index (weight in kg/(height in m)^2)")
-    diabetes_pedigree_function = models.FloatField(help_text="Diabetes pedigree function score")
-    age = models.PositiveIntegerField(help_text="Age of the patient (years)")
-    outcome = models.BooleanField(help_text="Diabetes outcome: 0 (No), 1 (Yes)")
+    pregnancies = models.PositiveIntegerField()
+    glucose = models.PositiveIntegerField()
+    blood_pressure = models.PositiveIntegerField()
+    skin_thickness = models.PositiveIntegerField()
+    insulin = models.PositiveIntegerField()
+    bmi = models.FloatField()
+    diabetes_pedigree_function = models.FloatField()
+    age = models.PositiveIntegerField()
+    outcome = models.BooleanField()
 
     def __str__(self):
         return f"Patient {self.id} - Outcome: {'Diabetes' if self.outcome else 'No Diabetes'}"
